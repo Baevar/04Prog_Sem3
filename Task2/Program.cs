6b4083e85281ed83ef5a373d6ddc4d427ba29f8d
baevar_laptop <baevar@mai.ru>
2024-03-20 14:42:27 +0700

    Мое решение Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте массив из 10 элементов, заполненный числами из
+промежутка [-10, 10]. Замените отрицательные элементы на
+положительные, а положительные на отрицательные.
+Пример
+[1 -5 6]
+=> [-1 5 -6]*/
+
+int[] arrNum = new int[] { -1, 9, 0, 4, -6, -4, 3, 7, -8, -10 };
+
+Console.WriteLine("Исходный массив данных:");
+
+foreach (int k in arrNum)
+{
+    Console.Write(k + " ");
+}
+
+Console.WriteLine("\nИнверсный массив данных:");
+
+int[] reverse = new int [arrNum.Length];
+
+for (int i = 0; i < arrNum.Length; i++)
+{
+reverse[i] = arrNum[i] * -1;
+Console.Write(reverse[i] + " ");
+}

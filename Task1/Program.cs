e1fb04f30794ee366d010b19246636811ace57d3
baevar_laptop <baevar@mai.ru>
2024-03-20 14:29:11 +0700

    Мой вариант Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задайте массив. Напишите программу, которая определяет,
+присутствует ли заданное число в массиве. Программа
+должна выдать ответ: Да/Нет.
+Примеры
+[1 3 4 19 3], 8 => Нет
+[-4 3 4 1], 3 => Да */
+
+int[] arrNum = new int[]{2,7,11,5,1,14,6,7};
+
+Console.WriteLine("Введите искомое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Исходный массив данных");
+
+int count = 0;
+for (int i=0; i< arrNum.Length; i++)
+{
+    Console.Write(arrNum[i] + " ");
+    if (num == arrNum[i])
+    {
+        count++;
+    }
+}
+if (count > 0)
+{
+Console.WriteLine("\nЗаданное число ПРИСУТСТВУЕТ в массиве данных");
+}
+else 
+{
+Console.WriteLine("\nЗаданное число ОТСУТСТВУЕТ в массиве данных");
+}
